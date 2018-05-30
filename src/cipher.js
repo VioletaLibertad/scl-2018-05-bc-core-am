cde0e4017e86ba53a65336369b6f4ef18ad5d105
Violeta <morada.libertad@gmail.com>
2018-05-29 20:12:07 -0400

    DOM base listo. HTML y CSS mejorados

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,10 @@
 window.cipher = {
   encode: () => {
-    /* Acá va tu código */
+    /* Acá va tu código. Estos son metodos del objeto cipher. 
+    Son acciones o comportamientos que puede tener un objeto*/
+    
   },
   decode: () => {
-    /* Acá va tu código */
+    /* Acá va tu código. Otro metodo*/
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-/* Acá va tu código */
+function code() {
+  let msgToCode = document.getElementById("codeBox").value;
+  document.getElementById("codedText").innerHTML = msgToCode;
+
+}
+
+function decode() {
+  let msgtoDecode = document.getElementById("decodeBox").value;
+  document.getElementById("decodedText").innerHTML = msgtoDecode;
+}
+
+
+
+
+/*
+const str = "HOLA ESTE ES UN STRING";
+let result = "";
+
+for = (let i = 0; i < str.length; i++) {
+
+console.log('Esta es la iteración número: ' + i + ',letra: ' + str[i]);
+
+result += str[i].toLowerCase();
+console.log('El nuevo string: '+ result);
+
+}*/
